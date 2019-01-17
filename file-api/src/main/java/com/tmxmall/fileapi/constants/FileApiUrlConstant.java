06a46ec9c5f2d4fb018d175d29168582f3f1f451
wangchongli <wangchongli@tmxmall.com>
2019-01-17 14:27:00 +0800

    添加URL常量

--- file-api/src/main/java/com/tmxmall/fileapi/constants/FileApiUrlConstant.java
+++ file-api/src/main/java/com/tmxmall/fileapi/constants/FileApiUrlConstant.java
@@ -2,7 +2,7 @@ package com.tmxmall.fileapi.constants;
 
 public class FileApiUrlConstant {
 
-    private static String[] hosts = new String[]{""};  // 配置请求的host
+    private static String[] hosts = new String[]{"http://api.tmxmall.com"};  // 配置请求的host
 
     private static String serverName = hosts[0];
 
